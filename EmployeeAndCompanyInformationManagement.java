1196fe9a301458693c69bdc10832dc04a02a6224
AnviMehta01 <anvim452@gmail.com>
2026-09-12 20:26:56 +0530

    Add files via upload

---
+++ EmployeeAndCompanyInformationManagement.java
@@ -0,0 +1,41 @@
+package week6assignment;
+
+public class EmployeeAndCompanyInformationManagement {
+
+    static class Employee {
+
+        String empName;
+        double salary;
+
+        static String companyName =
+                "Bright Horizon Technologies";
+
+        static int employeeCount = 0;
+
+        Employee(String empName, double salary) {
+            this.empName = empName;
+            this.salary = salary;
+            employeeCount++;
+        }
+
+        static void printCompanyInfo() {
+            System.out.println(companyName);
+            System.out.println("Employees on record: "
+                    + employeeCount);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee employee1 =
+                new Employee("Ravi", 40000);
+
+        Employee employee2 =
+                new Employee("Anita", 50000);
+
+        Employee employee3 =
+                new Employee("Karthik", 60000);
+
+        Employee.printCompanyInfo();
+    }
+}
